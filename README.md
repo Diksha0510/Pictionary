41657475997797f209a34dddd916f8e61dc5bf75
Kumar Ashwin <krashwin00@gmail.com>
2020-09-08 04:03:02 +0530

    Modifies Web UI

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 # Pictionary
-A real-time pictionary game against a bot.
+_A real-time pictionary game against a bot._
 
-**Load and Train Data**
+## Get Started!
+
+If you want to get this running and use the pre-trained model - my_model2.h5, type the following commands
+```shell
+export FLASK_APP=main.py
+flask run
+```
+
+**Dependencies:**
+* OpenCV (`pip install opencv-python`)
+* Pandas (`pip install pandas==0.22`)
+
+## Screenshot
+![HomePage Screenshot](./templates/home_page.png)
+
+## Load and Train your own data!
 
 Refer to the 'loadData_train.ipynb' for loading the data and training the neural network.
 
@@ -14,17 +29,14 @@ This model was trained on 40k images per pre-processed(full_......npy) file for 
 6. Belt
 7. Banana
 8. Cake
-
 The deep learning model has 2 convolutional layers and 1 GRU layer based on [1].
 
-**Install and Play the game**
-
-Execute the file 'pictionary.py' to start the game.
-
-Requirements:
-
-**References:**
+## References
 
 1. Xu, P., Song, Z., Yin, Q., Song, Y., & Wang, L. (2020). Deep Self-Supervised Representation Learning for Free-Hand Sketch. IEEE Transactions on Circuits and Systems for Video Technology, 1-1. doi:10.1109/tcsvt.2020.3003048
 
 2. [The Quick, Draw! Dataset](https://github.com/googlecreativelab/quickdraw-dataset)
+
+## Author
+**Diksha Choudhary**
+Email: diksha2.choudhary83@gmail.com
--- main.py
+++ main.py
@@ -11,23 +11,18 @@ app.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
 
 @app.route('/',methods=['POST','GET'])
 def index():
-    # return 'Hello world'
     pred=["alarm clock","bicycle","bed","airplane","apple","belt","banana","cake"]
+    # TODO: Take an list having the number and if you find the first number using random, remove that from the list and then find the other from (n-1) elements.
     i=pred[random.randint(0,7)]
     j=pred[random.randint(0,7)]
     k=pred[random.randint(0,7)]
     if request.method == 'GET':
-    # return '%s'%result
         return render_template('index.html', u=i,v=j,w=k)
-    # t=pictionary().run()
 
     if request.method == 'POST':
         draw=request.form.get("pict")
-        # print(draw)
-        # guess=values['guess']
-        # draw=values["draw"]
         if(draw == 'draw'):
-            result=subprocess.check_output('python3 /home/diksha/pictionary/begard.py', shell=True)
+            result=subprocess.check_output('python3 ./pictionary.py', shell=True)
             s=result.decode("utf-8").split()
             s=s[len(s)-2]
             return render_template('index.html', t=s, u=i,v=j,w=k)
@@ -36,20 +31,15 @@ def index():
 
 @app.route('/guess')
 def guess():
-    # return 'hello'
     images=["basket.gif","book.gif","jpg_to_gif.gif"]
-    str=images[random.randint(0,10)]
+    str = images[random.randint(0,3)]
     src = os.path.join(app.config['UPLOAD_FOLDER'],str)
-    # src=r'/home/diksha/python/jpg_to_gif.gif'
     return render_template('one.html', image=src)
 
 @app.route('/check',methods=['POST','GET'])
 def check():
     if request.method == 'POST':
         guess=request.form["guess"]
-        # if guess ==
-
-
 
 if __name__ == '__main__':
     app.run('localhost',5000,True)
